1f6307aa7e4af1f633237c254ae4caa141f5cd14
youanan <youanan@163.com>
2020-04-27 18:39:11 +0800

    也不知道更新了些啥？

---
+++ script/music_link.lua
@@ -0,0 +1,45 @@
+﻿
+require "import"
+import "android.content.Context"
+import "java.io.File"--导入File类
+import "android.widget.*"
+
+local op = (...)
+local word = tostring(service.getSystemService(Context.CLIPBOARD_SERVICE).getText()) --获取剪贴板 
+if op == nil then
+  local url = "http://www.eggvod.cn/music/?name="..word.."&type=ximalaya"
+else
+  local url = "http://www.eggvod.cn/music/?name="..op.."&type=ximalaya"
+end
+
+local headers = {
+  ["User-Agent"] = 'Mozilla/5.0 (Linux; Android 8.1.0; MI 8 SE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36',
+  ["Accept"] = "application/json, text/javascript, */*; q=0.01",
+  ["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8",
+  ["Referer"] = url,
+}
+
+local host = "http://www.eggvod.cn/music/"
+--Http.post(url,data,cookie,charset,header,callback)
+local data = {}
+data.input = word
+data.filter = "name"
+data.type = "ximalaya"
+data.page= "1"
+
+Http.post(host,data,nil,"utf-8",headers,function(a,b)
+
+--Http.get(url,nil,"utf-8",nil,function(a,b)
+if a==200 then 
+  --local json = require "cjson"
+  --local t = json.decode(b)
+  --local title = t.data[1].title
+  --local url = t.data[1].url
+  --service.getSystemService(Context.CLIPBOARD_SERVICE).setText(b)
+--写入剪贴板
+  service.openUrl(url)
+  print("成功!")
+
+else
+ print("网络似乎出了问题")
+end end)
--- wubi86_jidian_pinyin.lua
+++ wubi86_jidian_pinyin.lua
@@ -2,16 +2,13 @@
 require "import"
 
 function updateComposing(i,c,t)
-  if t == "笑话" then  --随机笑话
+  if t == "笑话" then  --随机在线笑话
     require "script.random_joke"
     jokes()
-  elseif t == "每日" then  --每日语句
+  elseif t == "每日" then  --在线每日语句
     require "script.daily_word"
     words()
-  elseif t == "鸡汤" then  --随机鸡汤
-    require "script.soul"
-    soul()
-  elseif i == "/ci" then  --随机诗词
+  elseif i == "/ci" then  --本地随机诗词
     require "script.poetry"
     poetry()
   elseif i == "/sc" then  --随机sentence
@@ -20,8 +17,5 @@ function updateComposing(i,c,t)
   elseif i == "/se" then  --随机sentence
     require "script.random_sentence"
     sentence()
-  elseif i == "/he"  then  --和风天气
-    require "script.heweather"
-    heweather()
   end
 end
